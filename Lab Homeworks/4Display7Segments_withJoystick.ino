de8b5e31f8a8812a6c01e0543e278bd6ed390d43
danadascalescu00 <48893255+danadascalescu00@users.noreply.github.com>
2019-11-21 14:04:28 +0200

    Update 4Display7Segments_withJoystick.ino

--- Lab Homeworks/4Display7Segments_withJoystick.ino
+++ Lab Homeworks/4Display7Segments_withJoystick.ino
@@ -7,52 +7,51 @@ const int pinF = 11;
 const int pinG = 6;
 const int pinDP = A2;
 const int pinD1 = 7;
-const int pinD2 = 9; 
+const int pinD2 = 9;
 const int pinD3 = 10;
 const int pinD4 = 13;
 
 const int pinSW = 4;
-const int pinX = A0;
-const int pinY = A1;
+const int pinX = A1;
+const int pinY = A0;
+
+int switchValue = 0;
+int xValue = 0;
+int yValue = 0;
 
 const int segSize = 8;
+
 const int noOfDisplays = 4;
 const int noOfDigits = 10;
-
-int switchValue;
-int xValue;
-int yValue;
 
 int dpState = LOW;
 int swState = LOW;
 
-bool swPressedFirstTime = false;
-bool swPressedSecondTime = true;
 bool joyMovedOx = false;
 bool joyMovedOy = false;
+int minTreshold = 400;
+int maxTreshold = 600;
 
-const int minThreashold = 400;
-const int maxThreashold = 600;
+int currentDisplay = 0;
 
-int currentDisplay = 0; //current digit on which I display
-
+bool swPressed = false;
+bool swPressed2 = true;
+ 
 int currentNumber[noOfDisplays] = {
   0, 0, 0, 0
 };
-
-int digits[noOfDisplays] = {
-  pinD1, pinD2, pinD3, pinD4
-};
-
 int currentDpState[noOfDisplays] = {
   600, 0, 0, 0
 };
-
 int segments[segSize] = {
   pinA, pinB, pinC, pinD, pinE, pinF, pinG, pinDP
 };
 
-const byte digitMatrix[noOfDigits][segSize - 1] = {
+int digits[noOfDisplays] = {
+  pinD1, pinD2, pinD3, pinD4
+ };   
+ 
+byte digitMatrix[noOfDigits][segSize - 1] = {
 // a  b  c  d  e  f  g
   {1, 1, 1, 1, 1, 1, 0}, // 0
   {0, 1, 1, 0, 0, 0, 0}, // 1
@@ -66,65 +65,58 @@ const byte digitMatrix[noOfDigits][segSize - 1] = {
   {1, 1, 1, 1, 0, 1, 1}  // 9
 };
 
-void showDigit(int number){
-  for(int i = 0; i < noOfDisplays; i++){
+void showDigit(int num) {
+  for (int i = 0; i < noOfDisplays; i++) {
     digitalWrite(digits[i], HIGH);
   }
-  digitalWrite(digits[number], LOW);
+  digitalWrite(digits[num], LOW);
 }
 
 void displayNumber(byte digit, int decimalPoint) {
-  for(int i = 0; i < segSize - 1; i++){
+  for (int i = 0; i < segSize - 1; i++) {
     digitalWrite(segments[i], digitMatrix[digit][i]);
   }
+
   analogWrite(segments[segSize - 1], decimalPoint);
 }
 
 void setup() {
+  for (int i = 0; i < segSize - 1; i++){
+    pinMode(segments[i], OUTPUT);  
+  }
+  for (int i = 0; i < noOfDisplays; i++){
+    pinMode(digits[i], OUTPUT);  
+  }
   pinMode(pinSW, INPUT_PULLUP);
+  pinMode(pinX, INPUT);
+  pinMode(pinY, INPUT);
   Serial.begin(9600);
 }
+
 
 void loop() {
 
   swState = digitalRead(pinSW);
 
-  if(swState == LOW)
-  {
-    if(swPressedFirstTime == false && swPressedSecondTime == true){
-      swPressedFirstTime = true;
-      swPressedSecondTime = false;   
-    }else if(swPressedFirstTime == true && swPressedSecondTime == true){
-      swPressedFirstTime = false;
-      swPressedSecondTime = false;    
-    }
-  }else{
-    swPressedSecondTime = true;
+  if(swState == LOW && swPressed == false && swPressed2 == true ){
+    swPressed = true;
+    swPressed2 = false;
+  }
+  else if(swState == LOW && swPressed == true && swPressed2 == true){
+    swPressed = false;
+    swPressed2 = false;
+  }
+  if(swState == HIGH){
+    swPressed2 = true;
   }
 
   xValue = analogRead(pinX);
-  
-  if(xValue <= minThreashold && joyMovedOx == false && swPressedFirstTime == false){
-    if(currentDisplay > 0){
-      currentDisplay--;
-      //change state of DP point
-      currentDpState[currentDisplay - 1] = 0;
-      currentDpState[currentDisplay] = 600;
-    }
-    else{
-      currentDpState[0] = 0;
-      currentDisplay = noOfDisplays - 1;
-      currentDpState[noOfDisplays - 1] = 600;
-    }
-    joyMovedOx = true;
-  }
 
-   if(xValue >= maxThreashold && joyMovedOx == false && swPressedFirstTime == false){
+  if (xValue < minTreshold && joyMovedOx == false && swPressed == false){
     if(currentDisplay < noOfDisplays){
       currentDisplay++;
-      //change state of DP point
-      currentDpState[currentDisplay - 1] = 0;
       currentDpState[currentDisplay] = 600;
+      currentDpState[currentDisplay - 1] = 0;
     }
     else{
       currentDisplay = 0;
@@ -132,15 +124,30 @@ void loop() {
       currentDpState[noOfDisplays - 1] = 0;
     }
     joyMovedOx = true;
+      
   }
-
-  if(xValue > minThreashold && xValue < maxThreashold){
+  
+  if(xValue >= maxTreshold && joyMovedOx == false && swPressed == false){
+    if(currentDisplay > 0){
+      currentDisplay--;
+      currentDpState[currentDisplay] = 600;
+      currentDpState[currentDisplay + 1] = 0;
+    }
+    else{
+      currentDisplay = noOfDisplays - 1;
+      currentDpState[currentDisplay] = 600;
+      currentDpState[0] = 0;
+    }
+    joyMovedOx = true;
+  }
+  
+  if(xValue >= minTreshold && xValue <= maxTreshold){
     joyMovedOx = false;
   }
 
   yValue = analogRead(pinY);
-
-  if(yValue <= minThreashold && joyMovedOy == false && swPressedFirstTime == true){
+        
+   if (yValue < minTreshold && joyMovedOy == false && swPressed == true){
     if(currentNumber[currentDisplay] > 0){
       currentNumber[currentDisplay]--;
     }
@@ -148,25 +155,26 @@ void loop() {
       currentNumber[currentDisplay] = 9;
     }
     joyMovedOy = true;
-  }
-
-  if(yValue >= maxThreashold && joyMovedOy == false && swPressedFirstTime == true){
-    if(currentNumber[currentDisplay] < 9){
-      currentNumber[currentDisplay]++;
-    }
-    else{
-      currentNumber[currentDisplay] = 0;
-    }
-    joyMovedOy = true;
    }
-
-  if(yValue > minThreashold && yValue < maxThreashold){
-    joyMovedOy = false;
-  }
+   
+   if(yValue >= maxTreshold && joyMovedOy == false && swPressed == true){
+     if(currentNumber[currentDisplay] < 9){
+       currentNumber[currentDisplay]++;
+     }
+     else{
+       currentNumber[currentDisplay] = 0;
+     }
+     joyMovedOy = true;
+   }
+   
+   if(yValue >= minTreshold && yValue <= maxTreshold){
+     joyMovedOy = false;
+   }
 
   for(int i = 0; i < noOfDisplays; i++){
     showDigit(i);
     displayNumber(currentNumber[i], currentDpState[i]);
-    delay(25);
+    delay(5);
   }
+
 }
